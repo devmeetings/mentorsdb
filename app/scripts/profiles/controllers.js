9f59ff3f3388a13ab6a440e0093bc203215421a4
Tomusdrw <tomusdrw@gmail.com>
2016-04-06 12:29:02 +0200

    Import/export

--- app/scripts/profiles/controllers.js
+++ app/scripts/profiles/controllers.js
@@ -1,12 +1,15 @@
 angular.module('App')
-.controller('ProfilesCtrl', ['$scope', 'Profiles', function($scope, Profiles) {
+.controller('ProfilesCtrl', ['$scope', '$window', 'Profiles', function($scope, $window, Profiles) {
 
     $scope.profiles = [];
     $scope.searchText = "";
 
-    Profiles.getAll().then(function(profiles) {
-        $scope.profiles = profiles;
-    });
+    function refreshProfiles() {
+      Profiles.getAll().then(function(profiles) {
+          $scope.profiles = profiles;
+      });
+    }
+    refreshProfiles();
 
     $scope.searchFunction = function(searchText) {
         var keywords = searchText.toLowerCase().split(' ');
@@ -21,6 +24,22 @@ angular.module('App')
             }
             return match;
         };
+    };
+
+    $scope.import = function(profiles) {
+      try {
+        profiles = JSON.parse(profiles);
+      } catch (e) {
+        $window.alert('Malformed data: ' + e);
+        return;
+      }
+
+      Profiles.import(profiles).then(function () {
+        $scope.importing = false;
+        refreshProfiles();
+
+        $window.alert('Import complete.');
+      });
     };
 
 }]);
