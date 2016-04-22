e4328bfc6a87c5f314654d8f22bdb85af397cf8b
Szymon Piwowarczyk <szymon.piwowarczyk@s3group.com>
2016-04-22 12:33:53 +0200

    F018 Layout

--- app/scripts/profiles/directives.js
+++ app/scripts/profiles/directives.js
@@ -4,6 +4,21 @@ angular.module('App').directive('profile', function() {
         scope: {
             profile: '='
         },
+        controller: ['$scope', function($scope) {
+
+            $scope.scoreGroup = function(score) {
+                var group;
+                if(score < -60) {
+                    group = 1;
+                } else if(score > 60) {
+                    group = 10;
+                } else {
+                    group = Math.floor((score + 60) / 13.3333333333);
+                }
+                return 'group' + group;
+            };
+
+        }],
         templateUrl: 'templates/profile.html'
     }
 })
