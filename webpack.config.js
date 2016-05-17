5079df1bb120eac3248fd89f090770c80c7bdc24
Szymon Piwowarczyk <szymon.piwowarczyk@sienn.pl>
2016-05-17 23:24:40 +0200

    F022 Webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -3,17 +3,15 @@ const path = require('path');
 const autoprefixer = require('autoprefixer');
 const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-var ExtractTextPlugin = require("extract-text-webpack-plugin");
-
-const isProdEnv = process.env.WEBPACK_ENV === 'production';
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 const config = {
 	entry: [
-		'./src/index'
+		'./src/base/app'
 	],
 	output: {
-		publicPath: '/',
-    path: path.resolve(__dirname, './dist'),
+		publicPath: './',
+    path: path.resolve(__dirname, './dist/base'),
     filename: 'bundle.js'
 	},
 	resolve: {
@@ -27,7 +25,6 @@ const config = {
 			{test: /\.js$/, loader: 'ng-annotate!babel', exclude: /node_modules/},
 			{test: /\.json$/, loader: 'json', exclude: /node_modules/},
 			{test: /\.html$/, loader: 'html'},
-      {test: /\.scss$/, loader: ExtractTextPlugin.extract('style' ,'css!sass!postcss')},
 			{test: /\.css$/, loader: ExtractTextPlugin.extract('style', 'css')},
       {test: /\.(gif|png|jpe?g|svg)$/i, loader: 'file?name=images/[name].[ext]'},
       {test: /(\.eot|\.woff|\.woff2|\.ttf)$/,loader: 'file?name=fonts/[name].[ext]'},
@@ -40,34 +37,17 @@ const config = {
     }),
     new webpack.NoErrorsPlugin(),
     new ExtractTextPlugin("styles.css", {
-        allChunks: true
-      })
+      allChunks: true
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      comments: false
+    }),
+    new CopyWebpackPlugin([{from: './src/index.html', to: 'index.html'}], {})
   ]
 };
-
-if (!isProdEnv) {
-	config.devtool = 'source-map';
-  config.plugins = config.plugins.concat([
-		new webpack.DefinePlugin({
-			'WEBPACK_ENV': '"dev"'
-		})
-	]);
-} else {
-  config.output.publicPath = './';
-	config.devtool = 'hidden-source-map';
-  config.plugins = config.plugins.concat([
-		new webpack.optimize.UglifyJsPlugin({
-			compress: {
-				warnings: false
-			},
-			comments: false
-		}),
-		new webpack.DefinePlugin({
-			'WEBPACK_ENV': '"production"'
-		}),
-		new CopyWebpackPlugin([{from: './src/index.html', to: 'index.html'}], {})
-	]);
-}
 
 module.exports = config;
 /* eslint-enable */
