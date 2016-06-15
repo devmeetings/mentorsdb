e12bc59314f8580d78e56cc2f5c3040a1a25ab6a
Szymon Piwowarczyk <szymon.piwowarczyk@sienn.pl>
2016-06-15 21:48:59 +0200

    F027 Linkedin find all

--- server/api/controllers/LinkedinController.js
+++ server/api/controllers/LinkedinController.js
@@ -1,19 +1,57 @@
 var firebase = require('firebase');
-firebase.initializeApp({
-  serviceAccount: "config/firebase.json",
-  databaseURL: 'https://mentorsdb.firebaseio.com',
-});
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    serviceAccount: "config/firebase.json",
+    databaseURL: 'https://mentorsdb.firebaseio.com',
+  });
+}
 
 module.exports = {
   index: function (req, res) {
-    console.log('index called');
-    return res.json({
-      todo: 'index() is not implemented yet!'
+    var params = req.params.all();
+    /*var query = {};
+    query.sort = 'score DESC';
+    Linkedin
+      .find(query)
+      .paginate({
+        page: +params.page,
+        limit: +params.limit || 3
+      })
+      .exec(function(err, data) {
+        res.json(data);
+      });*/
+    Linkedin.count().exec(function(err, count) {
+      var limit = +params.limit || 5;
+      var page = params.page ? Math.max(1, +params.page) : 1;
+      var skip = (page - 1) * limit;
+      var pageCount = Math.ceil(count / limit);
+      Linkedin.native(function(err, collection) {
+        collection
+          .find({
+            $where: function() {
+              return false;
+            }
+          })
+          .sort({
+            score: -1
+          })
+          .skip(skip)
+          .limit(limit)
+          .toArray(function(err, data) {
+            res.json({
+              count: count,
+              limit: limit,
+              page: page,
+              pageCount: pageCount,
+              data: data
+            });
+          });
+      });
     });
   },
 
   create: function (req, res) {
-    var params = req.params.all()
+    var params = req.params.all();
     Linkedin.create(params).exec(function(err, created) {
       return res.json(created);
     });
@@ -50,4 +88,3 @@ module.exports = {
     });
   }
 };
-
