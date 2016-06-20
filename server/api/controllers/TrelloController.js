1c2d0ffc2844b7691620d691b86089ec17d29209
Szymon Piwowarczyk <szymon.piwowarczyk@sienn.pl>
2016-06-20 22:36:02 +0200

    F027 Add card with tags to trello

--- server/api/controllers/TrelloController.js
+++ server/api/controllers/TrelloController.js
@@ -1,9 +1,13 @@
+require('es6-promise').polyfill();
 var Trello = require("trello");
 var trello = new Trello("ab3e486648b4909be5427ab953859e8a", "c5d4d43264e520d104151f44ec389cb473edbfe0b1f2ab8f8f1b05e40390fecd");
 
 module.exports = {
+  boardId: '57625508dc20256493fb9899',
+
   lists: function (req, res) {
-    trello.getListsOnBoard('M90S6AZb', function(err, data) {
+    var trelloController = this;
+    trello.getListsOnBoard(trelloController.boardId, function(err, data) {
       res.json(data.map(function(list) {
         return list.name;
       }));
@@ -11,27 +15,17 @@ module.exports = {
   },
 
   generate: function(req, res) {
+    var trelloController = this;
     Profile
       .find()
       .limit(5)
       .populate('linkedin')
       .exec(function(err, profiles) {
         var profile;
-        function pushCard(err, trelloCard) {
-          if(typeof trelloCard === 'object') {
-            Profile.update({
-              id: profile.id
-            }, {
-              trello: trelloCard.id
-            }).exec(function() {});
-          }
+        function pushCard() {
           if(profiles.length > 0) {
             profile = profiles.pop();
-            trello.addCard(profile.linkedin.name, profile.linkedin.comment, '5762552263f4a88b0e94c9f5', pushCard, {
-              pos: 'top',
-              due: null,
-              urlSource: profile.linkedin.img
-            });
+            trelloController.addCard(profile).then(pushCard);
           } else {
             res.json({
               "status": "done"
@@ -40,5 +34,51 @@ module.exports = {
         }
         pushCard();
       });
+  },
+
+  addCard: function(profile) {
+    var trelloController = this;
+    return new Promise(function(resolve, reject) {
+      trello.addCard(
+        profile.linkedin.name,
+        profile.linkedin.comment,
+        '5762552263f4a88b0e94c9f5',
+        function(err, trelloCard) {
+          Profile.update({
+            id: profile.id
+          }, {
+            trello: trelloCard.id
+          }).exec(function() {});
+          if(profile.linkedin.tags && profile.linkedin.tags.length > 0) {
+            trello.getLabelsForBoard(trelloController.boardId, function(err, labels) {
+              profile.linkedin.tags.map(function(tag) {
+                var label = labels.find(function(item) {
+                  return item.name === tag;
+                });
+                function addLabel(label) {
+                  trello.addLabelToCard(trelloCard.id, label.id);
+                }
+                if(label) {
+                  addLabel(label);
+                } else {
+                  trello.addLabelOnBoard(trelloController.boardId, tag, null, function(err, trelloLabel) {
+                    labels.push(trelloLabel);
+                    addLabel(trelloLabel);
+                  });
+                }
+              });
+              resolve(trelloCard);
+            });
+          } else {
+            resolve(trelloCard);
+          }
+        },
+        {
+          pos: 'top',
+          due: null,
+          urlSource: profile.linkedin.img
+        }
+      );
+    });
   }
 };
